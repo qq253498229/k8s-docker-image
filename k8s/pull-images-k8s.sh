05f804b36df705769c9c09258add0b6c81d8643a
wangbin <253498229@qq.com>
2019-03-03 04:29:43 +0800

    fix bug

--- k8s/pull-images-k8s.sh
+++ k8s/pull-images-k8s.sh
@@ -8,6 +8,6 @@ for image in ${images[@]}; do
   docker rmi registry.cn-beijing.aliyuncs.com/k8s-docker-image/$image
 done
 
-docker pull registry.cn-beijing.aliyuncs.com/k8s-docker-image/flannel
-docker tag registry.cn-beijing.aliyuncs.com/k8s-docker-image/flannel quay.io/coreos/flannel:v0.11.0-amd64
-docker rmi registry.cn-beijing.aliyuncs.com/k8s-docker-image/flannel
+docker pull registry.cn-beijing.aliyuncs.com/k8s-docker-image/flannel:v0.11.0-amd64 && \
+docker tag registry.cn-beijing.aliyuncs.com/k8s-docker-image/flannel:v0.11.0-amd64 quay.io/coreos/flannel:v0.11.0-amd64 && \
+docker rmi registry.cn-beijing.aliyuncs.com/k8s-docker-image/flannel:v0.11.0-amd64
