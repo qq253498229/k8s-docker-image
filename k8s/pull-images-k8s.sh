d2a3874e50ef077ba25fb68895e36011198740ad
王斌 <253498229@qq.com>
2019-03-16 01:44:03 +0800

    polish

--- k8s/pull-images-k8s.sh
+++ k8s/pull-images-k8s.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
-images=(kube-proxy:v1.13.3 kube-apiserver:v1.13.3 kube-controller-manager:v1.13.3 kube-scheduler:v1.13.3 coredns:1.2.6 etcd:3.2.24 pause:3.1)
+images=(
+kube-proxy:v1.13.4
+kube-apiserver:v1.13.4
+kube-controller-manager:v1.13.4
+kube-scheduler:v1.13.4
+coredns:1.2.6
+etcd:3.2.24
+pause:3.1
+)
 
 for image in ${images[@]}; do
   docker pull registry.cn-beijing.aliyuncs.com/k8s-docker-image/$image
@@ -15,3 +23,13 @@ docker rmi registry.cn-beijing.aliyuncs.com/k8s-docker-image/flannel:v0.11.0-amd
 docker pull registry.cn-beijing.aliyuncs.com/k8s-docker-image/nginx-ingress-controller:0.22.0
 docker tag registry.cn-beijing.aliyuncs.com/k8s-docker-image/nginx-ingress-controller:0.22.0 quay.io/kubernetes-ingress-controller/nginx-ingress-controller:0.22.0
 docker rmi registry.cn-beijing.aliyuncs.com/k8s-docker-image/nginx-ingress-controller:0.22.0
+
+docker pull registry.cn-qingdao.aliyuncs.com/wangdali/tiller:v2.12.3
+docker tag registry.cn-qingdao.aliyuncs.com/wangdali/tiller:v2.12.3 gcr.io/kubernetes-helm/tiller:v2.12.3
+docker rmi registry.cn-qingdao.aliyuncs.com/wangdali/tiller:v2.12.3
+
+
+#k8s.gcr.io/kube-proxy                                            v1.13.4
+#k8s.gcr.io/kube-controller-manager                               v1.13.4
+#k8s.gcr.io/kube-apiserver                                        v1.13.4
+#k8s.gcr.io/kube-scheduler                                        v1.13.4
