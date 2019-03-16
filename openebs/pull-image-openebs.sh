c39ce3e17d03262740369d1359d7137e4791aca2
王斌 <253498229@qq.com>
2019-03-16 23:30:49 +0800

    polish

--- openebs/pull-image-openebs.sh
+++ openebs/pull-image-openebs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-images=(node-disk-manager-amd64:v0.3.1 cstor-pool:0.8.1 cstor-pool-mgmt:0.8.1 cstor-istgt:0.8.1 m-exporter:0.8.1 m-apiserver:0.8.1 snapshot-provisioner:0.8.1 snapshot-controller:0.8.1 jiva:0.8.1 openebs-k8s-provisioner:0.8.1)
+images=(node-disk-manager-amd64:v0.3.1 cstor-pool:0.8.0 cstor-pool-mgmt:0.8.0 cstor-istgt:0.8.0 m-exporter:0.8.0 m-apiserver:0.8.0 snapshot-provisioner:0.8.0 snapshot-controller:0.8.0 jiva:0.8.0 openebs-k8s-provisioner:0.8.0)
 
 for image in ${images[@]}; do
   docker pull registry.cn-beijing.aliyuncs.com/k8s-docker-image/$image
